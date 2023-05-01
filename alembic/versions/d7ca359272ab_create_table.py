60d7389c428b51c348ed7230783e9b557a108f1b
Nirmal Pandey <nirmalpandey048@gmail.com>
2023-05-01 19:24:00 +0545

    setup migrations and database

---
+++ alembic/versions/d7ca359272ab_create_table.py
@@ -0,0 +1,24 @@
+"""create_table
+
+Revision ID: d7ca359272ab
+Revises: 
+Create Date: 2023-05-01 19:03:54.024943
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd7ca359272ab'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
